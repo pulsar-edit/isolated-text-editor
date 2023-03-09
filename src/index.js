fae02c5bf80a02ab29c1f2e07a9a32638eb64d58
Maurício Szabo <mauricio.szabo@gmail.com>
2023-03-09 16:40:49 -0300

    Some key events

--- src/index.js
+++ src/index.js
@@ -25,13 +25,41 @@ function createEditor() {
   return new TextEditor({buffer})
 }
 
+function editorKeyDown(editor) {
+  return (evt) => {
+    const {ctrlKey, altKey, shiftKey, code} = evt
+    console.log("CODE", code)
+    switch(code) {
+    case("Backspace"):
+      if(ctrlKey) editor.deleteToBeginningOfWord()
+      else editor.backspace()
+      break;
+    case("Enter"):
+      editor.insertNewline()
+      break;
+    case("ArrowUp"):
+      editor.moveUp()
+      break;
+    case("ArrowDown"):
+      editor.moveDown()
+      break;
+    case("ArrowLeft"):
+      if(ctrlKey) editor.moveToBeginningOfWord()
+      else editor.moveLeft()
+      break;
+    case("ArrowRight"):
+      if(ctrlKey) editor.moveToBeginningOfNextWord()
+      else editor.moveRight()
+      break;
+    }
+  }
+}
+
 async function main() {
   await superstring
   const editor = createEditor()
   const pane = document.querySelector('atom-pane')
+  editor.element.onkeydown = editorKeyDown(editor)
   pane.appendChild(editor.element)
-  // console.log()
 }
 main()
-// console.log(superstring)
-// console.log(createEditor())
