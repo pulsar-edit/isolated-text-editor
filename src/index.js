90619761a71dc5969f24c872114be4b0e49760bd
Natalia Koti <natalia.koti@vivadi.com.br>
2023-07-18 10:12:51 -0300

    refactor/text-editor-config

--- src/index.js
+++ src/index.js
@@ -1,65 +1,72 @@
-const originalLoad = require('module')._load;
-const { Emitter, Disposable, CompositeDisposable } =  require('event-kit')
-const { Point, Range } =  require('text-buffer')
+const { render } = require('solid-js/web');
+const HelloWorld = require('./text-editor.jsx').default;
 
-const myAtomObject = {
-  Emitter, Disposable, CompositeDisposable,
-  Point, Range
-};
-require('module')._load = function(request, parent, isMain) {
-  if (request === 'atom') {
-    return myAtomObject;
-  }
-  return originalLoad(request, parent, isMain);
-};
+render(HelloWorld, document.body);
+
+//const originalLoad = require('module')._load;
+//const { Emitter, Disposable, CompositeDisposable } =  require('event-kit')
+//const { Point, Range } =  require('text-buffer')
+
+//const myAtomObject = {
+//  Emitter, Disposable, CompositeDisposable,
+//  Point, Range
+//};
+//require('module')._load = function(request, parent, isMain) {
+//  if (request === 'atom') {
+//    return myAtomObject;
+//  }
+//  return originalLoad(request, parent, isMain);
+//};
 
 
-const TextEditor = require('./text-editor')
-const Clipboard = require('./clipboard')
-const TextBuffer = require('text-buffer')
-const {superstring} = require('superstring')
+//const TextEditor = require('./text-editor')
+//const Clipboard = require('./clipboard')
+//const TextBuffer = require('text-buffer')
+//const {superstring} = require('superstring')
 
-TextEditor.setClipboard(new Clipboard())
-function createEditor() {
-  const buffer = new TextBuffer()
-  return new TextEditor({buffer})
-}
+//TextEditor.setClipboard(new Clipboard())
+//function createEditor() {
+//  const buffer = new TextBuffer()
+//  return new TextEditor({buffer})
+//}
 
-function editorKeyDown(editor) {
-  return (evt) => {
-    const {ctrlKey, altKey, shiftKey, code} = evt
-    console.log("CODE", code)
-    switch(code) {
-    case("Backspace"):
-      if(ctrlKey) editor.deleteToBeginningOfWord()
-      else editor.backspace()
-      break;
-    case("Enter"):
-      editor.insertNewline()
-      break;
-    case("ArrowUp"):
-      editor.moveUp()
-      break;
-    case("ArrowDown"):
-      editor.moveDown()
-      break;
-    case("ArrowLeft"):
-      if(ctrlKey) editor.moveToBeginningOfWord()
-      else editor.moveLeft()
-      break;
-    case("ArrowRight"):
-      if(ctrlKey) editor.moveToBeginningOfNextWord()
-      else editor.moveRight()
-      break;
-    }
-  }
-}
+//function editorKeyDown(editor) {
+//  return (evt) => {
+//    const {ctrlKey, altKey, shiftKey, code} = evt
+//    console.log("CODE", code)
+//    switch(code) {
+//    case("Backspace"):
+//      if(ctrlKey) editor.deleteToBeginningOfWord()
+//      else editor.backspace()
+//      break;
+//    case("Enter"):
+//      editor.insertNewline()
+//      break;
+//    case("ArrowUp"):
+//      editor.moveUp()
+//      break;
+//    case("ArrowDown"):
+//      editor.moveDown()
+//      break;
+//    case("ArrowLeft"):
+//      if(ctrlKey) editor.moveToBeginningOfWord()
+//      else editor.moveLeft()
+//      break;
+//    case("ArrowRight"):
+//      if(ctrlKey) editor.moveToBeginningOfNextWord()
+//      else editor.moveRight()
+//      break;
+//    }
+//  }
+//}
 
-async function main() {
-  await superstring
-  const editor = createEditor()
-  const pane = document.querySelector('atom-pane')
-  editor.element.onkeydown = editorKeyDown(editor)
-  pane.appendChild(editor.element)
-}
-main()
+//async function main() {
+//  await superstring
+//  const editor = createEditor()
+//  const pane = document.querySelector('atom-pane')
+//  editor.element.onkeydown = editorKeyDown(editor)
+//  pane.appendChild(editor.element)
+//}
+//main()
+
+
--- start.js
+++ start.js
@@ -1,19 +1,45 @@
 const path = require('path')
 const {app, BrowserWindow} = require('electron')
 
-const exportsPath = path.join(__dirname, 'exports');
+function createWindow() {
+    const win = new BrowserWindow({
+        width: 900,
+        height: 600,
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js'),
+            nodeIntegration: true,
+            contextIsolation: false,
+        },
+    });
+    win.loadFile('./src/index.html');
+}
+
+app.whenReady().then(() => {
+    createWindow();
+
+    app.on('activate', function () {
+        if (BrowserWindow.getAllWindows().length === 0) createWindow();
+    });
+});
+
+app.on('window-all-closed', function () {
+    if (process.platform !== 'darwin') app.quit();
+});
+
+
+//const exportsPath = path.join(__dirname, 'exports');
 // console.log("PATHS", require('module').globalPaths)
 // require('module').globalPaths.push(exportsPath);
 // console.log("EXP", exportsPath)
 
 // console.log("ATOM", require('atom'))
-app.on('ready', () => {
-  let browser = new BrowserWindow({
-    width: 900, height: 600,
-    webPreferences: {
-      nodeIntegration: true,
-      contextIsolation: false,
-    }}
-  )
-  browser.loadURL(path.join('file://', __dirname, '/src/index.html'))
-})
+//app.on('ready', () => {
+//  let browser = new BrowserWindow({
+//    width: 900, height: 600,
+//    webPreferences: {
+//      nodeIntegration: true,
+//      contextIsolation: false,
+//    }}
+//  )
+//  browser.loadURL(path.join('file://', __dirname, '/src/index.html'))
+//})
